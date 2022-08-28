7a0fc8443388eb300e86e94666241a1997a758f0
Sebastian Contras <33270425+sebastiancontras@users.noreply.github.com>
2022-08-28 16:32:29 +0300

    questions - final version

---
+++ 7-questions/app.js
@@ -0,0 +1,27 @@
+// using selectors inside the element
+
+const questions = document.querySelectorAll('.question');
+
+questions.forEach(question => {
+  const btn = question.querySelector('.question-btn');
+  btn.addEventListener('click', el => {
+    questions.forEach(item => {
+      if (item !== question) {
+        item.classList.remove('show-text');
+      }
+    });
+    question.classList.toggle('show-text');
+  });
+});
+
+// traversing the dom
+/*
+const questionBtns = document.querySelectorAll('.question-btn');
+
+questionBtns.forEach(btn => {
+  btn.addEventListener('click', e => {
+    const question = e.currentTarget.closest('.question');
+    question.classList.toggle('show-text');
+  });
+});
+*/
